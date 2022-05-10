485226e36d818e1a0c0521a152ec9b3a547fae38
edwin carrillo <edwincarrillo9@outlook.com>
2022-05-10 19:50:09 -0400

    cool seed data from api script

--- app/seeds/gettinganime.py
+++ app/seeds/gettinganime.py
@@ -3,13 +3,15 @@ import requests
 
 def write():
     lines = []
-    for x in range(0, 500, 10):
+    names = []
+    for x in range(0, 2000, 10):
         response = requests.get(
-            f'https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]={x}&sort=ratingRank')
+            f'https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]={x}&sort=popularityRank')
         real = response.json()['data']
         for x in real:
-            if(x['attributes']['slug'] and x['attributes']['coverImage'] and x['attributes']['coverImage']['large'] and x['attributes']['episodeCount'] and x['attributes']['description']):
-                lines.append(f"{x['attributes']['slug']} = Anime(id = {x['id']}, name = '{x['attributes']['slug']}', picture= '{x['attributes']['coverImage']['large']}' episides = {x['attributes']['episodeCount']}, bio = '''{x['attributes']['description']}''')")
+            if(x['attributes']['slug'] and x['attributes']['posterImage'] and x['attributes']['episodeCount'] and x['attributes']['description']):
+                lines.append(f"{x['attributes']['slug'].replace('-','_')}= Anime(name='{x['attributes']['slug'].replace('-',' ')}', cover='{x['attributes']['posterImage']['large']}', episodes={x['attributes']['episodeCount']}, bio='''{x['attributes']['description']}''')")
+                names.append(x['attributes']['slug'].replace('-','_'))
         with open('anime.txt', 'w') as f:
             for line in lines:
                 f.write(line)
