0ddaf042d4c44b29cba35d2f283187be979e715d
ecarr <edwincarrillo9@outlook.com>
2022-06-11 18:24:57 -0400

    feed is rendering to home

--- react-app/src/components/activity_feed/index.js
+++ react-app/src/components/activity_feed/index.js
@@ -4,12 +4,25 @@ import { getting_feed } from "../../store/feed"
 
 const Activity = () => {
   const dispatch = useDispatch()
-  const feed = useSelector(state => state.feed.feed)
+  const feed = useSelector(state => state.feed)
+  console.log(feed)
   useEffect(() => {
     dispatch(getting_feed())
   }, [])
   return (
+    <>
     <h1>actively working on this feature</h1>
+    {feed.length ? feed.map(it => {
+      return (
+        <div>
+          <img src={it.anime.cover} height='100'/>
+          <h2>{it.user.username}</h2>
+          <div>{it.content}</div>
+        </div>
+        )
+    }) : null}
+    </>
+
   )
 }
 export default Activity
